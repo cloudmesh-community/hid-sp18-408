9f639563e20476f447f33eee59e9e5babd116792
manoj13691 <manoj.13691@gmail.com>
2018-04-15 03:46:13 -0400

    Updating Makefile

---
+++ swagger-docker/network/setup.py
@@ -0,0 +1,6 @@
+from distutils.core import setup
+
+setup(name='time', version='1.0', packages=[''], url='', license='', author='Manoj Joshi',
+    author_email='manjoshi.iu.edu',
+    description='This is a package for some network functionalities to be used in the REST service in '
+                'the course Cloud Computing.')
